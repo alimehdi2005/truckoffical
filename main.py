01f419677a36305d416a76e47e909ea8af1868b5
alimehdi2005 <117386046+alimehdi2005@users.noreply.github.com>
2022-12-31 07:33:19 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template('test.html')
+
+
+@app.route("/contact")
+def contact():
+    return render_template('index.html')
+
+
+@app.route("/book")
+def book():
+    return render_template('book.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
